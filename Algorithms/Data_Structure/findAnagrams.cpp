9f86fb03270fa0dee0d3ddd5872dc370d73f247e
khulaifi95 <just4xzd@icloud.com>
2020-05-09 00:29:48 +0100

    Algorithms data structure notes added.

---
+++ Algorithms/Data_Structure/findAnagrams.cpp
@@ -0,0 +1,34 @@
+vector<int> findAnagrams(string s, string t) {
+    vector<int> res;
+    int left = 0, right = 0;
+
+    unordered_map<char, int> needs;
+    unordered_map<char, int> window;
+    for (char c : t) needs[c]++;
+
+    int match = 0;
+
+    while (right < s.size()) {
+        char c1 = s[right];
+        if (needs.count(c1)) {
+            window[c1]++;
+            if (window[c1] == needs[c1])
+                match++;
+        }
+        right++;
+
+        while (match == needs.size()) {
+            if (right - left == t.size()) {
+                res.push_back(left);
+            }
+            char c2 = s[left];
+            if (needs.count(c2)) {
+                window[c2]--;
+                if (window[c2] < needs[c2])
+                    match--;
+            }
+            left++;
+        }
+    }
+    return res;
+}
