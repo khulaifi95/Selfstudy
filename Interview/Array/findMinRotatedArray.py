9914f2a7717408b8e5c995ef68d4ab37d68c81c9
khulaifi95 <just4xzd@icloud.com>
2020-08-21 02:38:18 +0800

    Interview array problems 'find min in rotated array' added.

---
+++ Interview/Array/findMinRotatedArray.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        l = 0
+        r = len(nums) - 1
+
+        while r > l:
+            mid = l + (r - l) // 2
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+
+        return nums[l]
